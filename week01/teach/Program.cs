1c5cbc477133bbce75d6b9efa72c15b6df2f386f
rorellana-b <ronalbalmoreorellanabonilla@192.168.1.9>
2025-07-06 15:50:37 -0600

    week01 assigment

--- week01/teach/Program.cs
+++ week01/teach/Program.cs
@@ -13,6 +13,6 @@ ArraySelector.Run();
 // Uncomment and run as you get to the solution part
 // Console.WriteLine("\n======================\nDivisors Solution\n======================");
 // DivisorsSolution.Run();
-//
+// //
 // Console.WriteLine("\n======================\nArray Selector Solution\n======================");
-// ArraySelectorSolution.Run();
+ArraySelectorSolution.Run();
